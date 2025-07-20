8f8eb3cc2b634f1d87c283318b71f5b260b81805
kadaici <anvarito2@gmail.com>
2025-07-21 00:53:08 +0600

    hw: hw4 added

---
+++ lessons/lesson3.py
@@ -0,0 +1,16 @@
+# from abc import ABC,  abstractmethod
+
+# class Animal(ABC):
+#     pass
+    
+#     @abstractmethod
+#     def step(self):
+#         pass
+    
+#     @abstractmethod
+#     def make_sound(self):
+#         pass
+    
+# class Dog(Animal):
+    
+        
